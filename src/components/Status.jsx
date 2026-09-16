2edee7aab43cf78afd5dd59fc15d56dceef07a42
Kati Stark <kati.stark95@gmail.com>
2026-09-16 14:12:02 +0100

    Updated Status losing prompt

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,7 +1,7 @@
 
 export default function Status(props) {
     const title=props.isGameWon ? "You win!" : "You lose!"
-    const description=props.isGameWon ? "Well done! 🎉" : "Better start learning Assembly! 🤷‍♂️"
+    const description=props.isGameWon ? "Well done! 🎉" : "Better luck next time!"
     return(
         <div className={props.isGameWon ? "status-container-win" : "status-container-lose"}>
             <h2 className="status-title">{title}</h2>
